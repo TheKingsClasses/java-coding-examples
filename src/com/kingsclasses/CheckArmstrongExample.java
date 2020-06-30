9d5e59fa36336d033f6b2301a07d826e21640bec
Raja Bhaiya Vishwakarma <rajasvnit51@gmail.com>
2020-06-30 09:49:43 +0530

    Armstrong Number Examples.

---
+++ src/com/kingsclasses/CheckArmstrongExample.java
@@ -0,0 +1,23 @@
+package com.kingsclasses;
+
+import java.util.Scanner;
+
+public class CheckArmstrongExample {
+	public static void main(String args[]) {
+		int number, temp, result = 0, rem;
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter any Positive Number : ");
+		number = scanner.nextInt();
+		scanner.close();
+		temp = number;
+		while (temp != 0) {
+			rem = temp % 10;
+			result = result + rem * rem * rem;
+			temp = temp / 10;
+		}
+		if (result == number)
+			System.out.print("Armstrong Number");
+		else
+			System.out.print("Not an Armstrong Number");
+	}
+}
---
+++ src/com/kingsclasses/CheckArmstrongNumberEx2.java
@@ -0,0 +1,61 @@
+package com.kingsclasses;
+
+import java.util.Scanner;
+
+/**
+ * @author Raja Vishwakarma 
+ * Java program to determine whether the number is Armstrong number or not.
+ */
+public class CheckArmstrongNumberEx2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter any Positive Number : ");
+		int number = scanner.nextInt();
+		scanner.close();
+		boolean isArmstrong = isArmstrong(number);
+		if (isArmstrong)
+			System.out.print(number + " is an Armstrong number.");
+		else
+			System.out.print(number + " is not an Armstrong number.");
+	}
+
+	/*
+	 * Method to calculate x raised to the power n
+	 */
+	public static int power(int x, long n) {
+		if (n == 0)
+			return 1;
+		if (n % 2 == 0)
+			return power(x, n / 2) * power(x, n / 2);
+		return x * power(x, n / 2) * power(x, n / 2);
+	}
+
+	/* Method to calculate number of digits in the number */
+	public static int getNumberOfDigits(int number) {
+		int numberOfDigits = 0;
+		while (number != 0) {
+			numberOfDigits++;
+			number = number / 10;
+		}
+		return numberOfDigits;
+	}
+
+	// Method to check whether the given number is Armstrong number or not
+	public static boolean isArmstrong(int number) {
+		// Calling getNumberOfDigits method to get the number of digits.
+		int numberOfDigits = getNumberOfDigits(number);
+		int temp = number, sum = 0;
+		while (temp != 0) {
+			int r = temp % 10;
+			sum = sum + power(r, numberOfDigits);
+			temp = temp / 10;
+		}
+
+		// If satisfies Armstrong condition
+		return (sum == number);
+	}
+}
